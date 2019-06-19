d0ea09b418e7bda2a9e9098f5b1be017be8bc9e0
D4lfonzo <dalfonzo18@gmail.com>
2019-06-19 10:00:33 -0400

    Agregada vista para escribir anuncios. Linkeada vista para leer anuncio

--- app/src/main/java/com/example/geschat/HomeFragment.java
+++ app/src/main/java/com/example/geschat/HomeFragment.java
@@ -1,8 +1,10 @@
 package com.example.geschat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -19,7 +21,30 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container,false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container,false);
+
+        TextView txt = (TextView) view.findViewById(R.id.readMore);
+        FloatingActionButton floatingActionButton = (FloatingActionButton) view.findViewById(R.id.btn_addAnnouncement);
+
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), AnnouncementActivity.class);
+                /*in.putExtra("some","somedata");*/
+                startActivity(in);
+            }
+        });
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), AddAnnouncementActivity.class);
+                startActivity(in);
+            }
+        });
+
+        return view;
 
     }
 }
