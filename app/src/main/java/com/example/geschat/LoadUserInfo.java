acde76b62369c89272e96ce90ff28e6c17ebae5e
Maria V <mariatua2@gmail.com>
2019-07-05 12:44:07 -0400

    saving profile picture to database storage

--- app/src/main/java/com/example/geschat/LoadUserInfo.java
+++ app/src/main/java/com/example/geschat/LoadUserInfo.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,9 +12,20 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.geschat.ui.login.LoginActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
 
@@ -41,6 +53,16 @@ public class LoadUserInfo extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 showImageChooser();
+            }
+
+        });
+
+        loadUserInformation();
+
+        findViewById(R.id.loadInfButtonSave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveUserInformation();
             }
         });
 
@@ -72,11 +94,91 @@ public class LoadUserInfo extends AppCompatActivity {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uriProfileImage);
                 imageView.setImageBitmap(bitmap);
 
-               // uploadImageToFirebaseStorage();
+               uploadImageToFirebaseStorage();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
+
+
+    private void loadUserInformation() {
+        final FirebaseUser user = auth.getCurrentUser();
+
+        if (user != null) {
+            if (user.getPhotoUrl() != null) {
+                Glide.with(this)
+                        .load(user.getPhotoUrl().toString())
+                        .into(imageView);
+            }
+
+            if (user.getDisplayName() != null) {
+                editText.setText(user.getDisplayName());
+            }
+
+        }
+    }
+
+
+    private void saveUserInformation() {
+
+
+        String displayName = editText.getText().toString();
+
+        if (displayName.isEmpty()) {
+            editText.setError("Name required");
+            editText.requestFocus();
+            return;
+        }
+
+        FirebaseUser user = auth.getCurrentUser();
+
+        if (user != null && profileImageUrl != null) {
+            UserProfileChangeRequest profile = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(displayName)
+                    .setPhotoUri(Uri.parse(profileImageUrl))
+                    .build();
+
+            user.updateProfile(profile)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Profile Updated", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void uploadImageToFirebaseStorage() {
+        final StorageReference profileImageRef = FirebaseStorage.getInstance().getReference("profilePhotos/" + System.currentTimeMillis() + ".jpg");
+
+        if (uriProfileImage != null) {
+            progressBar.setVisibility(View.VISIBLE);
+            profileImageRef.putFile(uriProfileImage)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressBar.setVisibility(View.GONE);
+                            //profileImageUrl = taskSnapshot.getDownloadUrl().toString();
+                            profileImageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    profileImageUrl= uri.toString();
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
 
 }
