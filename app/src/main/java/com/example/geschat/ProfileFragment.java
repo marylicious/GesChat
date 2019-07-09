3dcf75044b3b6351c4180e21f98b84763f801084
D4lfonzo <dalfonzo18@gmail.com>
2019-07-08 20:31:52 -0400

    profile update

--- app/src/main/java/com/example/geschat/ProfileFragment.java
+++ app/src/main/java/com/example/geschat/ProfileFragment.java
@@ -11,10 +11,20 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.geschat.adapters.ProfileNextChatAdapter;
 import com.example.geschat.adapters.ProfilePreviousChatAdapter;
 import com.example.geschat.models.ProfileNextPrevChat;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -22,6 +32,11 @@ public class ProfileFragment extends Fragment {
 
     ArrayList<ProfileNextPrevChat> listaNext;
     ArrayList<ProfileNextPrevChat> listaPrev;
+    private FirebaseUser user;
+    private FirebaseAuth auth;
+    private DatabaseReference db;
+    TextView userProfileName;
+    ImageView userProfilePict;
 
 
     @Nullable
@@ -30,6 +45,10 @@ public class ProfileFragment extends Fragment {
         setHasOptionsMenu(true);
         View view = inflater.inflate(R.layout.fragment_profile, container,false);
 
+        //BASE DE DATOS
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        db = FirebaseDatabase.getInstance().getReference();
 
         listaNext = new ArrayList<>();
         listaPrev = new ArrayList<>();
@@ -46,6 +65,13 @@ public class ProfileFragment extends Fragment {
         llenar2();
         ProfilePreviousChatAdapter adaptador2 = new ProfilePreviousChatAdapter(listaPrev);
         rv.setAdapter(adaptador2);
+
+        //PARA SETEAR COSAS DEL PERFIL
+        userProfileName = view.findViewById(R.id.userProfileName);
+        userProfilePict = view.findViewById(R.id.userProfilePict);
+
+        loadUserInformation();
+
 
         return view;
 
@@ -71,6 +97,49 @@ public class ProfileFragment extends Fragment {
 
             menu.findItem(R.id.cambiarFoto).setVisible(true);
         }
+
+
+    }
+
+    private void loadUserInformation(){
+
+        if (user != null) {
+            userProfileName.setText(user.getEmail());
+
+            //cargar nombre del usuario
+
+            if(user.getDisplayName() == null){
+
+                DatabaseReference userRef = db.child("Users").child(user.getUid()).child("name");
+
+
+                userRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String name = dataSnapshot.getValue(String.class);
+                        userProfileName.setText(name);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                    }
+                });
+
+            } else {
+                userProfileName.setText(user.getDisplayName());
+            }
+
+            //cargar foto del usuario
+            if(user.getPhotoUrl() != null){
+                Glide.with(this)
+                        .load(user.getPhotoUrl().toString())
+                        //.fitCenter()
+                        .centerCrop()
+                        .into(userProfilePict);
+            }
+
+        }
+
 
 
     }
